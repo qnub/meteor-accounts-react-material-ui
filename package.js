889895a2271aa5e4a5514c5348dc55cda154613d
Vadim Lopatyuk <qnub.ru@gmail.com>
2015-11-18 11:26:14 +0600

    Publication

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'React Material UI components for accounts.',
-  version: '0.13.3',
+  version: '0.13.3_1',
   name: 'qnub:accounts-react-material-ui',
   git: 'https://github.com/qnub/meteor-accounts-react-material-ui.git',
 });
